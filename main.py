801896cf74982f3f848280525ee5a661c9fd52fa
cytotoxin <georgedong9043@gmail.com>
2025-02-25 11:36:39 -0600

    added black screen, close function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame
+pygame.init()
 from constants import *
-def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-if __name__ == "__main__":
-    main()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+BLACK = (0, 0, 0)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(BLACK)
+    pygame.display.flip()
+
